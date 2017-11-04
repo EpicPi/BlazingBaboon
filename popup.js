bab068e6e2b9a339ad590a816a5e2d4dad7e13f8
William Zheng <williamzhengw@yahoo.com>
2017-11-04 15:11:28 -0400

    garbage

---
+++ popup.js
@@ -0,0 +1,45 @@
+  (function() {
+    'use strict';
+    var video = document.querySelector('video')
+      , canvas;
+    var arrays = [];
+    var count = 0;
+    /**
+     *  generates a still frame image from the stream in the <video>
+     *  appends the image to the <body>
+     */
+    function takeSnapshot() {
+      var img = document.querySelector('img') || document.createElement('img');
+      var context;
+      var width = video.offsetWidth
+        , height = video.offsetHeight;
+
+      canvas = canvas || document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+
+      context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0, width, height);
+      arrays.push(img);
+      img.src = canvas.toDataURL('image/png');
+      document.body.appendChild(img);
+      count += 1;
+      console.log(arrays)
+    }
+
+    // use MediaDevices API
+    // docs: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+    if (navigator.mediaDevices) {
+      // access the web cam
+      navigator.mediaDevices.getUserMedia({video: true})
+      // permission granted:
+        .then(function(stream) {
+          video.srcObject = stream;
+          video.addEventListener('click', takeSnapshot);
+        })
+        // permission denied:
+        .catch(function(error) {
+          document.body.textContent = 'Could not access the camera. Error: ' + error.name;
+        });
+    }
+  })();
